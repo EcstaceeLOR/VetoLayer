c858d5d7805a2bbdba3772cf41e315afe5fe3694
DemolaCodes <abdulmuizademola9@gmail.com>
2026-09-24 15:08:24 +0100

    feat(core): export Decision Receipt APIs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,4 +3,5 @@ export const CORE_PACKAGE = "@vetolayer/core" as const;
 export * from "./examples";
 export * from "./orchestrator";
 export * from "./parse";
+export * from "./receipts";
 export * from "./schemas";
